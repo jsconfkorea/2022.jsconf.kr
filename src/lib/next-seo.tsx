697345d61a6a463c51c31acd479f25fccb084d7b
Chris <ysm0622@gmail.com>
2022-07-21 06:18:45 +0900

    change og image url

--- src/lib/next-seo.tsx
+++ src/lib/next-seo.tsx
@@ -2,18 +2,20 @@ import { DefaultSeoProps } from 'next-seo'
 
 export const title = 'JSConf Korea 2022 🌈'
 
+export const URL = 'https://2022.jsconf.kr'
+
 export const SEO: DefaultSeoProps = {
   titleTemplate: `%s | ${title}`,
   defaultTitle: title,
 
   openGraph: {
     type: 'website',
-    url: 'https://2022.jsconf.kr',
+    url: URL,
     site_name: title,
     title,
     images: [
       {
-        url: '/thumb.png',
+        url: `${URL}/thumb.png`,
         alt: title,
         type: 'image/png',
         width: 1280,
