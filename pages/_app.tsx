0aceaacef1f7e0754afc5433a2cfaf9da3804472
Chris <ysm0622@gmail.com>
2022-07-21 15:02:05 +0900

    add canonical

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
         <link
           rel="apple-touch-icon"
           sizes="57x57"
--- src/pages/components/SEOProvider.tsx
+++ src/pages/components/SEOProvider.tsx
@@ -1,14 +1,21 @@
-import { SEO } from 'lib/next-seo'
+import { SEO, URL } from 'lib/next-seo'
 import { useTranslations } from 'next-intl'
 import { DefaultSeo } from 'next-seo'
+import { useRouter } from 'next/router'
 import { PropsWithChildren } from 'react'
+import { isServer } from 'utils/isServer'
 
 export function SEOProvider({ children }: PropsWithChildren<{}>) {
   const t = useTranslations('seo')
+  const { asPath } = useRouter()
 
   return (
     <>
-      <DefaultSeo {...SEO} description={t('description')} />
+      <DefaultSeo
+        {...SEO}
+        description={t('description')}
+        canonical={`${URL}${isServer() ? asPath : window.location.pathname}`}
+      />
       {children}
     </>
   )
