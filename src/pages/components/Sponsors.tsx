65035c4951e9aae586155907f96ab0097496c979
Sungmin Yang (Chris) <ysm0622@gmail.com>
2022-07-27 13:01:01 +0900

    Add gabia sponsor (#8)

--- src/pages/components/Sponsors.tsx
+++ src/pages/components/Sponsors.tsx
@@ -1,6 +1,5 @@
 import { useTranslations } from 'next-intl'
 import Image from 'next/future/image'
-import Link from 'next/link'
 import Fade from 'react-reveal/Fade'
 
 export const Sponsors = () => {
@@ -24,24 +23,46 @@ export const Sponsors = () => {
       <Fade bottom distance="1rem" duration={500} delay={100}>
         <div className="flex flex-col gap-4 sm:gap-8">
           <h2 className="text-2xl font-semibold">{t('Gold Sponsor')}</h2>
-          <Link href="https://imweb.me/">
-            <a className="self-center hover:opacity-70" target="_blank">
-              <Image
-                width={200}
-                height={100}
-                src="/imweb.png"
-                alt="imweb logo"
-                priority
-              />
-            </a>
-          </Link>
+          <a
+            href="https://imweb.me/"
+            className="self-center hover:opacity-70"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              width={200}
+              height={100}
+              src="/imweb.png"
+              alt="imweb logo"
+              priority
+            />
+          </a>
         </div>
       </Fade>
       <Fade bottom distance="1rem" duration={500} delay={200}>
         <div className="flex flex-col gap-4 sm:gap-8">
           <h2 className="text-2xl font-semibold">{t('Silver Sponsor')}</h2>
-          <Link href="https://www.labnote.co/">
-            <a className="self-center hover:opacity-70" target="_blank">
+          <div className="flex flex-col gap-4 self-center sm:flex-row sm:gap-12">
+            <a
+              href="https://company.gabia.com/"
+              className="hover:opacity-70"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <Image
+                width={200}
+                height={100}
+                src="/gabia.png"
+                alt="gabia logo"
+                priority
+              />
+            </a>
+            <a
+              href="https://www.labnote.co/"
+              className="hover:opacity-70"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
               <Image
                 width={200}
                 height={100}
@@ -50,23 +71,26 @@ export const Sponsors = () => {
                 priority
               />
             </a>
-          </Link>
+          </div>
         </div>
       </Fade>
       <Fade bottom distance="1rem" duration={700} delay={300}>
         <div className="flex flex-col gap-4 sm:gap-8">
           <h2 className="text-2xl font-semibold">{t('Bronze Sponsor')}</h2>
-          <Link href="https://linepluscorp.com/">
-            <a className="self-center hover:opacity-70" target="_blank">
-              <Image
-                width={200}
-                height={100}
-                src="/lineplus.png"
-                alt="lineplus logo"
-                priority
-              />
-            </a>
-          </Link>
+          <a
+            href="https://linepluscorp.com/"
+            className="self-center hover:opacity-70"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Image
+              width={200}
+              height={100}
+              src="/lineplus.png"
+              alt="lineplus logo"
+              priority
+            />
+          </a>
         </div>
       </Fade>
     </div>
