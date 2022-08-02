094dd9525451997a6364a894fdeb69218b0269d8
Chris <ysm0622@gmail.com>
2022-08-02 10:05:09 +0900

    ticket sold out text

--- src/pages/TicketPage.tsx
+++ src/pages/TicketPage.tsx
@@ -17,11 +17,16 @@ export const ticket = {
     ticket: '티켓',
     'korean-ticket': '국내 결제 수단으로 티켓 구매',
     'foreign-ticket': '해외 결제 수단으로 티켓 구매',
+    'sold-out': '티켓이 매진되었습니다!',
+    'thank-you':
+      'JSConf Korea 2022를 향한 여러분의 관심과 성원에 감사드립니다.',
   },
   en: {
     ticket: 'Tickets',
     'korean-ticket': 'Buy with a KOREAN payment method',
     'foreign-ticket': 'Buy with a NON-KOREAN payment method',
+    'sold-out': 'Tickets are SOLD OUT!',
+    'thank-you': 'Thank you for your love and support for JSConf Korea 2022.',
   },
 }
 
@@ -55,21 +60,19 @@ export default function TicketPage({ recordMap, pageName }: PageProps) {
 
         <div className="mt-16 flex w-full flex-1 flex-col pt-16">
           <Fade bottom distance="1rem" duration={700}>
-            <div className="flex items-center justify-center gap-2 sm:gap-4">
-              <Image
-                src="/ticket.png"
-                width={40}
-                height={40}
-                alt="Ticket icon"
-              />
+            <div className="flex flex-col items-center justify-center gap-8 sm:gap-12">
               <h2 className="text-4xl font-bold">{t('ticket')}</h2>
+              <div className="flex flex-col items-center gap-4 text-center">
+                <span className="text-xl font-semibold">{t('sold-out')}</span>
+                <span className="text-lg">{t('thank-you')}</span>
+              </div>
             </div>
           </Fade>
           <div className="mt-16 flex flex-col items-center justify-center gap-6 px-6">
             <Fade bottom distance="1rem" duration={700} delay={100}>
               <a
                 href="https://festa.io/events/e8QWVbxEybNJbJozFGgND6"
-                className="btn btn-block px-2 normal-case sm:btn-lg sm:w-[420px]"
+                className="btn btn-block bg-white px-2 normal-case text-black hover:bg-[#ffffff88] sm:btn-lg sm:w-[420px]"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -79,7 +82,7 @@ export default function TicketPage({ recordMap, pageName }: PageProps) {
             <Fade bottom distance="1rem" duration={700} delay={200}>
               <a
                 href="https://ti.to/jsconfkorea/jsconf-korea-2022"
-                className="btn btn-block px-2 normal-case sm:btn-lg sm:w-[420px]"
+                className="btn btn-outline btn-block border-white px-2 normal-case text-white hover:bg-white hover:text-black sm:btn-lg sm:w-[420px]"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -89,7 +92,7 @@ export default function TicketPage({ recordMap, pageName }: PageProps) {
           </div>
           <Fade bottom distance="1rem" duration={700} delay={300}>
             <NotionRenderer
-              className="!bg-black"
+              className="mt-16 !bg-black"
               recordMap={recordMap}
               fullPage
               darkMode
